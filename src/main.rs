942221e0a79fb447a5b82900a26a08de95589b44
Stephanie Aelmore <steph.aelmore@gmail.com>
2023-03-05 10:13:35 -1000

    window setup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use bevy::app::App;
+use bevy::input::InputPlugin;
+use bevy::prelude::*;
+use bevy::window::WindowPlugin;
+use bevy::winit::WinitPlugin;
 
 fn main() {
     App::new()
-        .add_system(hello_world)
+        .add_plugin(CorePlugin::default())
+        .add_plugin(InputPlugin::default())
+        .add_plugin(WindowPlugin {
+            window: WindowDescriptor { 
+                width: 640.0, 
+                height: 480.0,
+                title: "Sweeper 3D".to_string(),
+                ..default()
+            },
+            ..default()
+        })
+        .add_plugin(WinitPlugin::default())
+        .add_startup_system(setup)
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
+fn setup() {
+
 }
