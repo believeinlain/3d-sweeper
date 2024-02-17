d403192e289995e9b5ed83b00d1a5ddee5effd59
= <=>
2024-02-16 18:33:48 -1000

    click on block

--- src/block.rs
+++ src/block.rs
@@ -1,8 +1,10 @@
 use std::f32::consts::{FRAC_PI_4, PI};
 
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy_rapier3d::prelude::*;
 
-use bevy::input::mouse::MouseMotion;
+use bevy::input::mouse::{MouseButtonInput, MouseMotion};
 use bevy::render::mesh::VertexAttributeValues;
 
 #[derive(Component)]
@@ -11,11 +13,25 @@ struct Block;
 const UVS_HIDDEN: [[f32; 2]; 4] = [[0.0, 0.0], [0.25, 0.0], [0.25, 1.0], [0.0, 1.0]];
 const _UVS_1: [[f32; 2]; 4] = [[0.25, 0.0], [0.5, 0.0], [0.5, 1.0], [0.25, 1.0]];
 
+#[derive(Debug)]
+struct Ray {
+    position: Vec3,
+    direction: Vec3,
+}
+impl Ray {
+    fn new(position: Vec3, direction: Vec3) -> Self {
+        Self {
+            position,
+            direction,
+        }
+    }
+}
+
 pub struct BlockPlugin;
 impl Plugin for BlockPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn)
-            .add_systems(Update, rotate_with_mouse);
+            .add_systems(Update, (rotate_with_mouse, click_on_block));
     }
 }
 
@@ -51,10 +67,11 @@ fn spawn(
         PbrBundle {
             mesh,
             material,
-            transform,
+            transform: transform.clone(),
             ..default()
         },
         Block,
+        Collider::cuboid(0.5, 0.5, 0.5),
     ));
 }
 
@@ -84,4 +101,57 @@ fn rotate_with_mouse(
     } else {
         ev_motion.clear();
     }
+}
+
+fn click_on_block(
+    mut mouse_input: EventReader<MouseButtonInput>,
+    windows: Query<&Window, With<PrimaryWindow>>,
+    cameras: Query<(&Camera, &GlobalTransform)>,
+    rapier_context: Res<RapierContext>,
+) {
+    let Some(cursor_pos) = windows.single().cursor_position() else {
+        return;
+    };
+    let (camera, camera_trans) = cameras.single();
+    for mouse_event in mouse_input.read() {
+        if mouse_event.state.is_pressed() {
+            trace!("Left click at {cursor_pos:?}");
+            let Some(ray) = get_cursor_ray(camera, camera_trans, cursor_pos) else {
+                continue;
+            };
+            trace!("Cursor ray at {ray:?}");
+            let max_toi = 10.0;
+            let solid = true;
+            let filter = QueryFilter::new();
+
+            let Some(collider) =
+                rapier_context.cast_ray(ray.position, ray.direction, max_toi, solid, filter)
+            else {
+                continue;
+            };
+            info!("Collider entity {collider:?} hit");
+        }
+    }
+}
+
+fn get_cursor_ray(
+    camera: &Camera,
+    camera_trans: &GlobalTransform,
+    cursor_pos: Vec2,
+) -> Option<Ray> {
+    let view = camera_trans.compute_matrix();
+
+    let viewport = camera.logical_viewport_rect()?;
+    let screen_size = camera.logical_target_size()?;
+    let adj_cursor_pos = cursor_pos - Vec2::new(viewport.min.x, screen_size.y - viewport.max.y);
+
+    let projection = camera.projection_matrix();
+    let far_ndc = projection.project_point3(Vec3::NEG_Z).z;
+    let near_ndc = projection.project_point3(Vec3::Z).z;
+    let cursor_ndc = (adj_cursor_pos / viewport.size()) * 2.0 - Vec2::ONE;
+    let ndc_to_world: Mat4 = view * projection.inverse();
+    let near = ndc_to_world.project_point3(cursor_ndc.extend(near_ndc));
+    let far = ndc_to_world.project_point3(cursor_ndc.extend(far_ndc));
+    let ray_direction = far - near;
+    Some(Ray::new(near, ray_direction))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::*;
+use bevy::{log::LogPlugin, prelude::*, window::WindowResolution};
+use bevy_rapier3d::prelude::*;
 
 mod block;
 mod camera;
@@ -8,7 +9,25 @@ use camera::MainCameraPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resolution: WindowResolution::new(800.0, 600.0),
+                        title: "3D Sweeper".to_string(),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(LogPlugin {
+                    level: bevy::log::Level::INFO,
+                    ..default()
+                }),
+        )
+        .add_plugins((
+            RapierPhysicsPlugin::<NoUserData>::default(),
+            RapierDebugRenderPlugin::default(),
+        ))
         .add_systems(Startup, setup)
         .add_plugins((BlockPlugin, MainCameraPlugin))
         .run();
